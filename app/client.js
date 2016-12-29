0ee5cc0113eb51b47852f2dd5d573cf1568f8a0c
Dmitry Druganov <drud@drud.cz>
2016-12-29 12:03:30 +0100

    Fixes to use lastest 'babel-polyfill', 'react-dom' and 'react-router'

--- app/client.js
+++ app/client.js
@@ -1,16 +1,17 @@
 /* eslint-env browser */
 /* global process */
-import 'babel/polyfill';
+import 'babel-polyfill';
 import React from 'react';
-import BrowserHistory from 'react-router/lib/BrowserHistory';
-import HashHistory from 'react-router/lib/HashHistory';
+import { render } from 'react-dom';
+import * as ReactRouter from 'react-router';
+import { browserHistory, hashHistory } from 'react-router';
 import Root from './Root';
 
 const history = (process.env.NODE_ENV === 'production')
-  ? new BrowserHistory()
-  : new HashHistory();
+  ? browserHistory
+  : hashHistory;
 
-React.render(
+render(
   <Root {...{ history }} />,
   document.getElementById('app')
 );
